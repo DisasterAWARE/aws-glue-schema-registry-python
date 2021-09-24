d2192e7f4d0420a6d7d99499ea927063d504ac60
Corentin Debost <codebost@gmail.com>
2021-09-24 15:59:50 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AWS Glue Schema Registry for Python
+
+Partial port of [aws-glue-schema-registry](https://github.com/awslabs/aws-glue-schema-registry) for Python.
---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='aws-glue-schema-registry',
+    version='0.0.1',
+    author='Corentin Debost',
+    author_email='corentin.debost@tenefit.com',
+    description='Use the AWS Glue Schema Registry.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    python_requires='>3.6'
+)
