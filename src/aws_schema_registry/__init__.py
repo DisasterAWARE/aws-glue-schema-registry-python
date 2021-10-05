1a81919e7be90e778679e7dec56bc91fe636e433
Corentin Debost <codebost@gmail.com>
2021-10-04 17:07:17 -0700

    Prepare release 1.0.0rc3

--- src/aws_schema_registry/__init__.py
+++ src/aws_schema_registry/__init__.py
@@ -7,7 +7,7 @@ from .serde import (
     DataAndSchema, SchemaRegistryDeserializer, SchemaRegistrySerializer
 )
 
-__version__ = '1.0.0rc2'
+__version__ = '1.0.0rc3'
 
 __all__ = [
     'CompatibilityMode',
